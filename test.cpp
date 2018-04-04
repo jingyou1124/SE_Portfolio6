f3de6fc490b37d6bcf6f21b30d2a948f6a9fb9a7
jingyou1124 <jingyoulim@hotmail.com>
2018-04-04 23:57:01 +0800

    add word in test.cpp

--- test.cpp
+++ test.cpp
@@ -2,17 +2,25 @@
 using namespace std; 
 int main () 
 {
-        int no1, no2, result,mul; 
+
 		cout<<"hello world"<<endl;
+=======
+        int no1, no2, sum,mul; 
         cout << "\nEnter the first number = " ; 
         cin  >> no1 ;
         cout << "\nEnter the second number = " ;
         cin  >> no2 ; 
-        result = no1 + no2 ;
+        
+		
+		sum = no1 + no2 ;
+		
+		//this is multiple
 		mul = no1 * no2;
+		
         cout << "\nThe sum of "<< no1 <<" and "<< no2;
-		cout << "is = "<<result;
+		cout << "is = "<< sum;
 		cout << "\nThe mul of "<< no1 <<" and "<< no2;
 		cout << "is = "<<mul;
-        return 0 ;
+		return 0;
 }
+
