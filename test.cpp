d86bc001ca18bc4b161715fb08f19b8833f7544f
hengcc0125 <hengcc0125@gmail.com>
2018-04-04 23:16:44 +0800

    add mul

--- test.cpp
+++ test.cpp
@@ -2,13 +2,16 @@
 using namespace std; 
 int main () 
 {
-        int no1, no2, result; 
+        int no1, no2, result,mul; 
         cout << "\nEnter the first number = " ; 
         cin  >> no1 ;
         cout << "\nEnter the second number = " ;
         cin  >> no2 ; 
         result = no1 + no2 ;
+		mul = no1 * no2;
         cout << "\nThe sum of "<< no1 <<" and "<< no2;
 		cout << "is = "<<result;
+		cout << "\nThe mul of "<< no1 <<" and "<< no2;
+		cout << "is = "<<mul;
         return 0 ;
 }
