fce604519410e0a268182e20701dc221dad3117c
jingyou1124 <jingyoulim@hotmail.com>
2018-04-04 23:00:49 +0800

    upload test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main () 
+{
+        int no1, no2, result; 
+        cout << "\nEnter the first number = " ; 
+        cin  >> no1 ;
+        cout << "\nEnter the second number = " ;
+        cin  >> no2 ; 
+        result = no1 - no2 ;
+        cout << "\nThe subtraction of "<< no1 <<" and "<< no2 <<" = "<< sum <<'\n' ;
+        return 0 ;
+}
