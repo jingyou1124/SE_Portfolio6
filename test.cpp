30a129c1840fbede163a3d572f0c2e594b4b0e08
jingyou1124 <jingyoulim@hotmail.com>
2018-04-04 23:05:25 +0800

    modified test.cpp

--- test.cpp
+++ test.cpp
@@ -2,12 +2,13 @@
 using namespace std; 
 int main () 
 {
-        int no1, no2, sum; 
+        int no1, no2, result; 
         cout << "\nEnter the first number = " ; 
         cin  >> no1 ;
         cout << "\nEnter the second number = " ;
         cin  >> no2 ; 
-        sum = no1 + no2 ;
-        cout << "\nThe subtraction of "<< no1 <<" and "<< no2 <<" = "<< sum <<'\n' ;
+        result = no1 + no2 ;
+        cout << "\nThe sum of "<< no1 <<" and "<< no2;
+		cout << "is = "<<result;
         return 0 ;
 }
